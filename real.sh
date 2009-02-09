f0a80ce883807866178d898f3cc008fc59493d0d
Max Lapan <max.lapan@gmail.com>
2009-02-09 23:28:48 +0300

    We won't solve real-size problem on local machine.

--- real.sh
+++ real.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tablix2 -n 1 -d 2 -i /home/shmuma/tmp/tablix/lib/tablix2/ real.xml
+tablix2 -n 16 -d 2 -i /home/shmuma/tmp/tablix/lib/tablix2/ real.xml
 tablix2_output efim result0.xml -i /home/shmuma/tmp/tablix/lib/tablix2/ > 1.html
