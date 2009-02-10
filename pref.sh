2eacccf1f3898238545b86225c0186836e3dab9e
Max Lapan <max.lapan@gmail.com>
2009-02-10 23:21:29 +0300

    Preferences module started.

--- pref.sh
+++ pref.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tablix2 -n 2 -d 3 -i /home/shmuma/tmp/tablix/lib/tablix2/ real.xml
+tablix2 -n 2 -d 4 -i /home/shmuma/tmp/tablix/lib/tablix2/ preference.xml
 tablix2_output efim result0.xml -i /home/shmuma/tmp/tablix/lib/tablix2/ > 1.html
