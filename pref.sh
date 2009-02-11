1e813c652649d4d0ff722b80dc0cd7240ae6e85d
Max Lapan <max.lapan@gmail.com>
2009-02-12 01:23:42 +0300

    New stuff.

--- pref.sh
+++ pref.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tablix2 -n 2 -d 4 -i /home/shmuma/tmp/tablix/lib/tablix2/ preference.xml
-tablix2_output efim result0.xml -i /home/shmuma/tmp/tablix/lib/tablix2/ > 1.html
+tablix2 -n 2 -d 3 -i /home/shmuma/tmp/tablix/lib/tablix2/ preference.xml
+tablix2_output -s time_description=time_descr.txt efim result0.xml -i /home/shmuma/tmp/tablix/lib/tablix2/ > 1.html
--- room.sh
+++ room.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 tablix2 -n 2 -d 3 -i /home/shmuma/tmp/tablix/lib/tablix2/ room_avail.xml
-tablix2_output htmlcss result0.xml -i /home/shmuma/tmp/tablix/lib/tablix2/ > 1.html
+tablix2_output efim result0.xml -i /home/shmuma/tmp/tablix/lib/tablix2/ > 1.html
